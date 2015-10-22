eb34ab5b2148cc1d1c801c305f17dc78cb54ec10
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-10-22 19:23:38 -0400

    Adding math file ad string file separately

--- luigi/string_encoding.py
+++ luigi/string_encoding.py
@@ -24,15 +24,3 @@ def decode(lst):
     for character, count in lst:
         q += character * count
     return q
-
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
